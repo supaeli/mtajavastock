9e8a7ca4988fbac4a0fcd48df74881e8c0481cc5
supaeli <elay17@gmail.com>
2014-11-25 17:48:09 +0200

    exe 4 , still need to figure: presenting all on the same page.

--- src/com/mta/elisproject/Stock.java
+++ src/com/mta/elisproject/Stock.java
@@ -1,6 +1,6 @@
 package com.mta.elisproject ;
 import java.util.Date ;
-
+import java.text.SimpleDateFormat;
 
 public class Stock {
 	private float bid ;
@@ -26,8 +26,9 @@ public class Stock {
 	public void setAsk(float assignedAsk) {//ask setter
 		ask = assignedAsk ;
 	}
-	public Date getDate() {//date getter
-		return date;
+	public String getDate(){
+		SimpleDateFormat format1 = new SimpleDateFormat("MM-dd-yyyy");  
+		return format1.format(this.date);
 	}
 	public void setDate(Date assignedDate) {//date setter
 		date = assignedDate ;
@@ -39,17 +40,9 @@ public class Stock {
 		symbol = assignedSymbol ;
 	}
 	public String getHtmlDescription() {
-		String symbol, date, combined ;
-		symbol = new String() ;
-		date = new String() ;
+		String  combined ;
 		
-		symbol = getSymbol().toString() ;//  need for 'symbol=new String' ??? 
-		//and also to the other other String objects in the method 
-		ask = getAsk() ;
-		bid = getBid() ;
-		date = getDate().toString() ;
-		// defined 4 'sentence' parts then combine to final 'combined string' in HTML format 
-		combined =  new String("<h1>symbol ask:"+ask+", bid:"+bid+" date</h1>") ;//how to include a String inside brackets??
+		combined =  new String("<b>Symbol</b>"+getSymbol()+"<b> ask: </b>" +getAsk()+", <b>bid: </b>"+getBid()+" <b>Date </b>"+ getDate()) ;//how to include a String inside brackets??
 		return combined;//last line
 		
 	}
--- src/com/mta/elisproject/StockDetailsServlet.java
+++ src/com/mta/elisproject/StockDetailsServlet.java
@@ -20,16 +20,17 @@ public class StockDetailsServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/html");
 	
-//here we write shit
+
 		Calendar c =  Calendar.getInstance() ;
 		
 		Date date = new Date() ;
 		c.set(2014, 11, 15) ;
 		date = c.getTime() ;
-		Stock stock1 = new Stock(//data from exe details) ;
-		Stock stock2 = new Stock(//data from exe details) ;
-		Stock stock3 = new Stock(//data from exe details) ;
+		Stock stock1 = new Stock((float)13.1, (float)12.4, date, "PIH" );//data from exe details) ;
+		Stock stock2 = new Stock((float)5.78, (float)5.5, date, "AAL");//data from exe details) ;
+		Stock stock3 = new Stock((float)31.2, (float)31.5, date, "CAAS");//data from exe details) ;
 		// call for getHTML for each stock, use res.*
+		resp.getWriter().println(stock1.getHtmlDescription()+"<br> "+stock2.getHtmlDescription()+"<br> "+stock3.getHtmlDescription()) ;
 	}
 	
 }
